ded2879966f9caf0dd919e1435a0e0908af729e7
Lucas Luiz <lucas.luiz10498@gmail.com>
2022-09-20 23:24:05 -0300

    versao com as imagens importadas

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import estadoInicial from "./assets/forca0.png"
+import estado1erro from "./assets/forca1.png"
+import estado2erro from "./assets/forca2.png"
+import estado3erro from "./assets/forca3.png"
+import estado4erro from "./assets/forca4.png"
+import estado5erro from "./assets/forca5.png"
+import estadoFinal from "./assets/forca6.png"
+export default function App (){
+    return(
+        <>
+        <div>
+            <img src={estadoInicial} />
+        </div>
+        
+        
+        </>
+    )
+}
