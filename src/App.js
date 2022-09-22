d61b3efbe45ea58f60842bc58031ecd3382e86f9
Lucas Luiz <lucas.luiz10498@gmail.com>
2022-09-21 23:54:34 -0300

    versao com css implementado

--- src/App.js
+++ src/App.js
@@ -5,22 +5,32 @@ import estado3erro from "./assets/forca3.png"
 import estado4erro from "./assets/forca4.png"
 import estado5erro from "./assets/forca5.png"
 import estadoFinal from "./assets/forca6.png"
+import palavras from "./Palavras"
 export default function App (){
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
+    let palavraSorteada = []
+    function getRandom() {      
+       let stringWord =(palavras[Math.floor(Math.random() * palavras.length)])
+       for(let i = 0; i < stringWord.length; i++){
+        palavraSorteada.push(stringWord[i])
+       }
+       console.log(palavraSorteada)
+    }
     return(
         <>
         <div className="container">
-            <div className="forcaEescolherPalavra">
+            <div className="corpo">
             <img src={estadoInicial} />
             <div className="botaoEunderline">
-            <button className="escolherPalavra" type="button">Escolher palavra</button>
-                <div className="palavraAserAdivinhada"></div>
+            <button className="escolherPalavra" onClick={getRandom} type="button">Escolher palavra</button>
+                <div className="palavraAserAdivinhada">
+                <span></span>
+                </div>
             </div>
             </div>
-            <div className="teclado">
+            <div className="tecladoVirtual">
                 {alfabeto.map((v)=>
-                <button className="botao">{v}</button>
+                <button className="teclas">{v}</button>
                 )}
             </div>
             <div>
